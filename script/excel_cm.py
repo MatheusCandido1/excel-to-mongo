84cb163c32291a7467ecc06613a7e2c91a963d26
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-01 11:14:26 -0300

    add script file

---
+++ script/excel_cm.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 26 16:15:41 2019
+
+@author: Matheus
+"""
+
+import xlrd
+import MySQLdb
+
+book = xlrd.open_workbook("example3.xls")
+
+sheet = book.sheet_by_name("new2")
+
+database = MySQLdb.connect(host="localhost", user = "root", passwd = "root", db = "emej")
+
+cursor = database.cursor()
+
+query = """INSERT INTO cpf_cm(cpf, ej) VALUES (%s,%s)"""
+
+for r in range(1, sheet.nrows):
+        cpf = sheet.cell(r,0).value 
+        ej = sheet.cell(r,1).value 
+
+        values = (cpf,ej)
+
+        cursor.execute(query, values)
+        
+cursor.close()
+
+database.commit()
+
+database.close()
