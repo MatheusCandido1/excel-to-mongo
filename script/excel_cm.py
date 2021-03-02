165a643382875bfa58b7912dba941fb0dea0f009
Gabriel Maximiniano <gabmaxsan@gmail.com>
2021-03-02 11:05:22 -0300

    add array with values

--- script/excel_cm.py
+++ script/excel_cm.py
@@ -13,6 +13,9 @@ book = xlrd.open_workbook("../sheets/Livre-Bleu/2021/livre-bleu-au-20210201.xls"
 
 sheet = book.sheet_by_name("Nomenclature")
 
+titles = []
+data = []
+
 # database = MySQLdb.connect(host="localhost", user = "root", passwd = "root", db = "emej")
 
 # cursor = database.cursor()
@@ -21,14 +24,18 @@ sheet = book.sheet_by_name("Nomenclature")
 
 # Print columns title
 for col in range (0, sheet.ncols):
-    print(sheet.cell(0,col).value)
+    title = sheet.cell(0,col).value
+    titles.append(title)
 
 # Print cell value
 for row in range(1, sheet.nrows):
+    register = []
     for col in range (0, sheet.ncols):
-        print(sheet.cell(row,col).value)
+        cell = sheet.cell(row,col).value
+        register.append({titles[col]: cell})
+    data.append(register)
 
-
+print(data)
 #         cursor.execute(query, values)
         
 # cursor.close()
